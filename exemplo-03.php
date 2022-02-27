12c3f44c919d3d632eca7b9d45694dc5eca98c99
gabriel <gabrilrenhardtdosreis@gmail.com>
2022-02-27 20:31:25 -0300

    aprendemos a definir a permições para todas as pastas criadas no servidor que o sistema vai ter acesso

---
+++ exemplo-03.php
@@ -0,0 +1,13 @@
+<?php
+
+// 0-7 leitura, execução e gravação 1(execução), 2(gravação), 3(executar e gravar), 4(leitura), 5(leitura e execução), 6(leita e gravação) 7(tudo))
+// 0123 1(dono do servidor), 2(usuarios), 3(convidados)
+
+$pasta = "arquivos";
+$permição = "0775";
+
+if(!is_dir($pasta)) mkdir($pasta, $pasta);
+
+echo "diretório criado com sucesso";
+
+?>
