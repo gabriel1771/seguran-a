bd7246ec54ae51da30bd2787f9420c009a77371a
gabriel <gabrilrenhardtdosreis@gmail.com>
2022-06-04 21:37:32 -0300

    criei novamente o exemplo 2

--- exemplo-03.php
+++ exemplo-03.php
@@ -2,14 +2,17 @@
 <form action="" method="post">
     <input type="text" name="busca">
     <button type="submit">enviar</button>
+    
 </form>
 
 <?php
 
     if(isset($_POST['busca'])){
 
-        //echo strip_tags($_POST['busca'], "<h1><strong>");
-        echo htmlentities($_POST['busca']);
+        echo strip_tags($_POST['busca'], "<h1><strong>");
+        //echo htmlentities($_POST['busca']);
+        //echo $_POST['busca'];
+        
 
     }
 
